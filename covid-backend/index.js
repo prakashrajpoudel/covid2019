057dec536aacf36a277b48b5bb61789ff1c95d13
ppoudel <ppoudel@Prakashs-MBP.fios-router.home>
2020-04-21 15:30:23 -0400

    Added serverless code

--- covid-backend/index.js
+++ covid-backend/index.js
@@ -1,29 +1,26 @@
-var express = require('express');
-var app = express();
-var fs = require("fs");
-var cors = require('cors');
+'use strict';
 const axios = require('axios');
-
-app.use(cors());
-
-app.get('/report', function (req, res) {
-
-    let query = req.query.queryStr;
+module.exports.endpoint = (event, context, callback) => {
     let url = `https://api.rxivist.org/v1/papers?metric=downloads&timeframe=alltime`;
     axios({
         method:'get',
         url
     })
-    .then(function (response) {
-        res.send(JSON.stringify(response.data));
+    .then(function (res) {
+       const response = {
+         statusCode: 200,
+         headers: {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Credentials': true,
+         },
+         body: JSON.stringify(res.data)
+       };
+       callback(null, response);
     })
     .catch(function (error) {
         console.log(error);
+        callback(error);
     });
-})
+};
 
-var server = app.listen(8081, function () {
-   var host = server.address().address
-   var port = server.address().port
-   console.log("Example app listening at http://%s:%s", host, port)
-})
